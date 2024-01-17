36db8bbea62cec409a985e925a299fe5cdd6e95a
tejartr7 <raghusai126@gmail.com>
2024-01-17 23:53:36 +0530

    footer changes by rtr

--- app/contact/Page.tsx
+++ app/contact/Page.tsx
@@ -12,7 +12,7 @@ const Page = () => {
 
         {/* Google Map location */}
         <div className="md:w-1/2 mb-4 md:mb-0 flex flex-col items-center justify-center">
-          <span className='text-xl mb-2'>Our location</span>
+          <span className='text-xl mb-2 font-bold'>Our location</span>
           <iframe
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1902.996227797748!2d78.42294323857062!3d17.460075195859915!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bcb9107fedf4be7%3A0x393e5c11bb6e6d6b!2sApple%20pharmacy!5e0!3m2!1sen!2sin!4v1705515648765!5m2!1sen!2sin"
             width="100%"
