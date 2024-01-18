3e220a5eba13c2a50ebc1e6f458c7ecd4bed5adc
tejartr7 <raghusai126@gmail.com>
2024-01-18 18:29:55 +0530

    dev commits

--- app/footer/page.tsx
+++ app/footer/page.tsx
@@ -1,7 +1,9 @@
 
 const Page = () => {
   return (
-    <div>Page</div>
+    <div className="font-bold text-center text-2xl bg-black" style={{color:'#0cafe5'}}>
+      Designed & developed by <a href="https:rtrdev.me" target="_blank" style={{color:'white'}}><u>RTR</u></a>
+    </div>
   )
 }
 
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,13 @@
 import Image from 'next/image'
-import Header from '../app/header/page';
-import Subheader from '../app/subheader/Page';
-import Hero from '../app/hero/Page.jsx';
-import Brands from '../app/brands/Page';
-import Services from '../app/services/Page';
-import Testimonials from '../app/testimonials/Page.jsx';
-import Contact from '../app/contact/Page';
-import Dummy from '../app/dummy/Page';
+import Header from './header/page';
+import Subheader from './subheader/Page';
+import Hero from './hero/Page.jsx';
+import Brands from './brands/Page';
+import Services from './services/Page';
+import Testimonials from './testimonials/Page.jsx';
+import Contact from './contact/Page';
+import Dummy from './dummy/Page';
+import Footer from './footer/Page'
 export default function Home() {
   return (
     <div className='font-poppins'>
@@ -17,6 +18,7 @@ export default function Home() {
       <Services/>
       <Testimonials/>
       <Contact/>
+      <Footer/>
     </div>
   )
 };
